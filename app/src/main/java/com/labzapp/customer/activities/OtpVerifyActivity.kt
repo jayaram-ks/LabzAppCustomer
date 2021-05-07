50d7757e1d26f4ce29a77aa49957e99e6d828ee4
jks <jayaramksurendran@gmail.com>
2021-05-07 11:53:23 +0530

    font add

--- app/src/main/java/com/labzapp/customer/activities/OtpVerifyActivity.kt
+++ app/src/main/java/com/labzapp/customer/activities/OtpVerifyActivity.kt
@@ -1,11 +1,15 @@
 package com.labzapp.customer.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.labzapp.customer.databinding.ActivityOtpVerifyBinding
 
 class OtpVerifyActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityOtpVerifyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp_verify)
+        binding = ActivityOtpVerifyBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
